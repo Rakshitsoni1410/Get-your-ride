2e82a8ebca777bd3aa186448054bfbc931bc1f12
Rakshitson1410 <rakshitrsoni@gmail.com>
2025-05-20 19:27:44 +0530

    `Added user routes and JSON parsing middleware`

--- Backend/app.js
+++ Backend/app.js
@@ -5,7 +5,7 @@ dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const connectDb = require('./db/db');
-
+const userRoutes = require('./routes/user.routes');
 const app = express();
 
 // Connect to MongoDB
@@ -13,12 +13,13 @@ connectDb();
 
 // Middleware
 app.use(cors());
-//app.use(express.json()); // Recommended to parse JSON requests
-
+// app.use(express.json()); // Recommended to parse JSON requests
+app.use(express.json()); // Increase the limit to 50mb
 // Routes
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+app.use('/users', userRoutes);
 // Export the app
 module.exports = app;
