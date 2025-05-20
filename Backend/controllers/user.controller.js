f224d41a22217fbe41ddf2d071f705eeaccfdc69
Rakshitson1410 <rakshitrsoni@gmail.com>
2025-05-20 19:18:46 +0530

    `Added JWT secret to .env file and implemented user registration with validation and authentication in user.controller.js`

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,7 +1,22 @@
-const userModel = require('../models/user.model');
-
+const userModel = require("../models/user.model");
+const userService = require("../services/user.service");
+const { validateUser } = require("../utils/validation/user.validation");
 // Register a new user
 
-module.exports.registerUser = async (req, res,next) => {
-    
-}
+module.exports.registerUser = async (req, res, next) => {
+  const errors = validateUser(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+  const { firstname, lastname, email, password } = req.body;
+  const hashedPassword = await userService.hashPassword(password);
+  const user = await userService.createUser({
+    firstname,
+    lastname,
+    email,
+    password: hashedPassword,
+  });
+  const token = user.generateAuthToken();
+  res.status(201).json({ token,user });
+
+};
