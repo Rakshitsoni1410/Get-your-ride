b23751f3f0a3d33ebb303f3d5c58757f1e94d029
Rakshitson1410 <rakshitrsoni@gmail.com>
2025-05-22 00:15:57 +0530

    "Removed .gitignore file in Backend directory"

---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "../assets/logo.PNG"; // Adjust the path as necessary
+const Home = () => {
+  return (
+    <div className="h-screen flex justify-between flex-col w-full bg-red-400">
+      <img src="\logo.png" alt="Logo" />
+      <div className="bg-white p-4 text-center">
+        <h2 className="text-xl font-semibold">Get started with Get-your-ride</h2>
+        <button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded">Continue</button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
