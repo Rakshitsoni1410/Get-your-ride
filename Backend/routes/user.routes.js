6365840e902eb7679d0ab621b87fdc0e0e5793a4
Rakshitson1410 <rakshitrsoni@gmail.com>
2025-05-21 13:00:09 +0530

    `Added login functionality to user controller and routes`

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {body} = require('express-validator');
 const userController = require('../controllers/user.controller');
+const { route } = require('../app');
 
 router.post('/register', [
     body('email').isEmail().withMessage('Invalid email address'),
@@ -10,4 +11,9 @@ router.post('/register', [
 ],
 userController.registerUser);
 
+router.post('/login', [
+    body('email').isEmail().withMessage('Invalid email address'),
+    body('password').isLength({min: 6}).withMessage('Password must be at least 6 characters long'),
+]
+, userController.loginUser);
 module.exports = router;
