643b52968408092a204a7e402a0b0ab5033ac555
Rakshitson1410 <rakshitrsoni@gmail.com>
2025-05-22 21:40:03 +0530

    `Added className to logo image in Home component`

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import logo from "../assets/logo.PNG"; // Adjust the path as necessary
 const Home = () => {
   return (
     <div className="h-screen flex justify-between flex-col w-full bg-red-400">
-      <img src="\logo.png" alt="Logo" />
+      <img src="\logo.png" alt="Logo" className="top-0 left-0 m-9 w-19 h-19 object-contain" />
       <div className="bg-white p-4 text-center">
         <h2 className="text-xl font-semibold">Get started with Get-your-ride</h2>
         <button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded">Continue</button>
