34bf6632447a11768c5555c3e65927851508454c
Rakshitson1410 <rakshitrsoni@gmail.com>
2025-05-26 13:13:33 +0530

    `Update react-router-dom to version 7.6.1 in package-lock.json and package.json, and add Link component to Home.jsx`

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import logo from "../assets/logo.PNG"; // Adjust the path as necessary
+import { Link } from "react-router-dom";
 const Home = () => {
   return (
-    <div className="h-screen flex justify-between flex-col w-full bg-red-400">
-      <img src="\logo.png" alt="Logo" className="top-0 left-0 m-9 w-19 h-19 object-contain" />
-      <div className="bg-white p-4 text-center">
-        <h2 className="text-xl font-semibold">Get started with Get-your-ride</h2>
-        <button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded">Continue</button>
+    <div>
+      <div className="bg-cover bg-center bg-[url('https://th.bing.com/th?q=Traffic+Signal+Light+Cartoon&w=120&h=120&c=1&rs=1&qlt=90&cb=1&dpr=1.3&pid=InlineBlock&mkt=en-IN&cc=IN&setlang=en&adlt=moderate&t=1&mw=247')] h-screen pt-8 flex justify-between flex-col w-full">
+        <img src="\logo.png" alt="Logo" className="w-16 ml-8 " />
+        <div className="bg-white pb-7  py-4 px-4">
+          <h2 className="text-3xl font-bold">Get started with Get-your-ride</h2>
+          <Link to='/login'className="flex items-center justify-center w-full bg-black text-white py-3 rounded mt-5">Continue</Link>
+        </div>
       </div>
     </div>
   );
