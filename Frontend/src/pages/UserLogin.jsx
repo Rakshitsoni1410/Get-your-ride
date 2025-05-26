006e5e30d7c0f3083b646eb134d994a96b7274cd
Rakshitson1410 <rakshitrsoni@gmail.com>
2025-05-26 15:27:59 +0530

    `Added React Router Link and useState hook to UserLogin component, added form fields for email and password, and a login button.`

--- Frontend/src/pages/UserLogin.jsx
+++ Frontend/src/pages/UserLogin.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const UserLogin = () => {
+  const [email, setEmail] = useState('')
+   const [password, setPassword] = useState('')
+   const submitHandler = () => {
+    console.log("hello");  
+   }
   return (
-    <div>UserLogin</div>
+    <div className='p-7 h-screen flex flex-col justify-between'>
+      <div>
+        <img src="\logo.png" alt="Logo" className="w-16 mb-10 " />
+
+        <from  onSubmit={submitHandler}>
+          <h3 className='text-lg font-medium mb-2'>What's your email</h3>
+          <input
+           value={email}
+           onChange={(e)=>{
+           setEmail(e.target.value) 
+           }}
+            className='bg-[#eeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base'
+            type="email"
+            placeholder="r@gmail.com"
+            required
+          />
+          <h3 className='text-lg font-medium mb-2'> Enter password</h3>
+          <input
+            value={password}
+            onChange={(e)=>{
+              setPassword(e.target.value)
+            }}
+            className='bg-[#eeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base'
+            type="password"
+            placeholder="password"
+            required />
+          <button
+            className='bg-[#111] text-white font-semibold mb-3 rounded px-4 py-2  w-full text-lg'
+          >Login</button>
+           </from>
+        <p className='text-center'> New here?
+            <Link to='/signup' className='text-blue-600'>
+              Create new account
+            </Link>
+          </p>
+      </div>
+      <div>
+        <button className='bg-[#10b461] text-white font-semibold mb-7 rounded px-4 py-2  w-full text-lg'
+        >Sign in  as captain  </button>
+      </div>
+    </div>
   )
 }
 
